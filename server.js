f3361c2a3450f0f122c4dff85cd8afb8add56ff7
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-15 17:29:58 +0530

    shorturl created using express

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express=require("express")
+const mongoose=require("mongoose")
+const shorturl=require("./models/shorturl")
 const app=express()
 
-app.set("view engine","ejs")
+mongoose.connect('mongodb://localhost/urlShortener',{
+    useNewUrlParser:true,useUnifiedTopology:true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
 
-app.get("/",(req,res)=>{
-    res.render('index')
+app.set("view engine","ejs")
+app.use(express.urlencoded({extended:false}))
+
+app.get("/",async(req,res)=>{
+    const shorturls=await shorturl.find()
+    res.render('index',{shorturls:shorturls})
+})
+
+app.post('/shorturls',async(req,res)=>{
+    await shorturl.create({full:req.body.shorturl})
+    res.redirect('/')
 })
 
 app.listen(process.env.PORT||3000)
