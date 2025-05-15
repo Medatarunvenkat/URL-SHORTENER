10563689b361e427c3998e25bc2bf250ebf76903
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-15 17:42:26 +0530

    Shortener done

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express=require("express")
 const mongoose=require("mongoose")
-const shorturl=require("./models/shorturl")
+const shorturl=require("./models/shorturl");
 const app=express()
 
-mongoose.connect('mongodb://localhost/urlShortener',{
-    useNewUrlParser:true,useUnifiedTopology:true,
+mongoose.connect('mongodb://localhost/urlshortener', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.error("MongoDB connection error:", err));
 
@@ -18,8 +20,17 @@ app.get("/",async(req,res)=>{
 })
 
 app.post('/shorturls',async(req,res)=>{
-    await shorturl.create({full:req.body.shorturl})
+    await shorturl.create({full:req.body.fullurl})
     res.redirect('/')
+})
+
+app.get('/:shorturl',async(req,res)=>{
+    const url=await shorturl.findOne({short:req.params.shorturl})
+    if(url==null) return res.sendStatus(404)
+
+    url.clicks++
+    await url.save()
+    res.redirect(url.full)
 })
 
 app.listen(process.env.PORT||3000)
